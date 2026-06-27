7222464d59b29042d242ddffc8318b27bca07ef0
Lee Hobson <core2lee@gmail.com>
2026-06-27 12:29:52 +0100

    adds a version.h

---
+++ src/version.h
@@ -0,0 +1,8 @@
+#ifndef VERSION_H__
+#define VERSION_H__
+
+/* Single source of truth for the plugin version string, shared by main.c (boot
+ * banner, first-run credit) and osd.c (the DEBUG overlay line). */
+#define BB_VERSION "0.91"
+
+#endif
